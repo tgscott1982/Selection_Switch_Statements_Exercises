adf7e8e13d56a9841ea73431541db052025b4000
Travis Gersch <t_gersch@hotmail.com>
2022-07-06 23:49:48 -0500

    selection statements first commit for e1 and e2

---
+++ Selection_Statements/Program.cs
@@ -0,0 +1,28 @@
+﻿#region Selection Statements Exercise 1
+
+
+var r = new Random();
+var favNumber = r.Next(1, 1000);
+int userInput;
+
+do
+{
+Console.WriteLine("Please pick a number between 1 and 1000.");
+userInput = int.Parse(Console.ReadLine());
+
+    if (userInput < favNumber)
+
+    {
+        Console.WriteLine($"{userInput} is too low.");
+    }
+    else if (userInput > favNumber)
+    {
+        Console.WriteLine($"{userInput} is too high.");
+    }
+    else
+    {
+        Console.WriteLine("Heyyyyy, you got it, yo!");
+    }
+} while (userInput != favNumber);
+
+#endregion
---
+++ Switch_Statements/Program.cs
@@ -0,0 +1,32 @@
+﻿#region Switch Statements Exercise 2
+
+Console.WriteLine("Hi! What's your favorite school subject?");
+
+string subject = Console.ReadLine();
+
+switch (subject.ToLower())
+{
+    case "math":
+        Console.WriteLine("Ugh, math.....J/K! I actually really like numbers!");
+        break;
+    case "english":
+        Console.WriteLine("I should've been an English teacher. Or a professional book-reader! *sigh*");
+        break;
+    case "gym":
+        Console.WriteLine("I was a fat kid.....fat kids don't do gym.");
+        break;
+    case "history":
+        Console.WriteLine("I'm terrible with dates!");
+        break;
+    case "geography":
+        Console.WriteLine("I can barely remember where my car keys are let alone tell you where something is on a map!");
+        break;
+    default:
+        Console.WriteLine("What? What's that? What kind of goofy school did you go to?");
+        break;
+
+
+
+}
+
+#endregion
